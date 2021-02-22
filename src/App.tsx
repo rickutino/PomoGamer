931c162b6a216f62a1fa0f566c90ed0dbc8180a9
rickutino <rickutino@gmail.com>
2021-02-22 22:18:43 +0900

    add var color in global.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import React from 'react';
-import { Button } from './components/Button';
 import './styles/global.css';
 
 function App() {
   return (
     <div className="App">
-      <Button color="red">Botão 1</Button>
-      <Button color="blue">Botão 2</Button>
-      <Button color="yellow">Botão 3</Button>
     </div>
   );
 }
