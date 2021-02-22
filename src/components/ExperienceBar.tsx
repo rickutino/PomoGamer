eb53cbfc69b821f42e6ee50862d2d903d43513e5
rickutino <rickutino@gmail.com>
2021-02-22 23:21:23 +0900

    add ExperienceBar components

---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ExperienceBar: React.FC = () => {
+  return (
+    <>
+      <span>0</span>
+      <div></div>
+      <span>600</span>
+    </>
+  )
+}
+
+export default ExperienceBar;
